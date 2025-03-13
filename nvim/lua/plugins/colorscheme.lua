960ae5961662c2c88356e7ad239e252c94222be6
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-03-13 00:39:49 -0400

    updated colorscheme, default handling

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,17 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
     "sainnhe/sonokai",
     priority = 1000,
     config = function()
       vim.g.sonokai_transparent_background = "1"
       vim.g.sonokai_enable_italic = "1"
       vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
+      -- vim.cmd.colorscheme("sonokai")
     end,
   },
 }
