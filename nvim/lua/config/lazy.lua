ed5f04148634b2dc814b09bb7b84eef27584cee8
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-03-14 01:24:38 -0400

    colorscheme updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "sonokai",
+      colorscheme = "tokyonight",
     } },
     -- import/override with your plugins
     { import = "plugins" },
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,7 +11,14 @@ return {
       vim.g.sonokai_transparent_background = "1"
       vim.g.sonokai_enable_italic = "1"
       vim.g.sonokai_style = "andromeda"
-      -- vim.cmd.colorscheme("sonokai")
     end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
   },
 }
