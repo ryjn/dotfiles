26d3ea3940e69336a27359684334dd0c5b4a8afb
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-03-20 20:43:06 -0400

    scrollEOF config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -55,3 +55,18 @@ require("lazy").setup({
     },
   },
 })
+
+-- Default settings
+require("scrollEOF").setup({
+  -- The pattern used for the internal autocmd to determine
+  -- where to run scrollEOF. See https://neovim.io/doc/user/autocmd.html#autocmd-pattern
+  pattern = "*",
+  -- Whether or not scrollEOF should be enabled in insert mode
+  insert_mode = true,
+  -- Whether or not scrollEOF should be enabled in floating windows
+  floating = true,
+  -- List of filetypes to disable scrollEOF for.
+  disabled_filetypes = {},
+  -- List of modes to disable scrollEOF for. see https://neovim.io/doc/user/builtin.html#mode()
+  disabled_modes = {},
+})
