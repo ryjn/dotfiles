e26a9032c1ae586a9d830ad2f5883730d712e4fa
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2025-03-20 23:44:50 -0400

    confom config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -23,8 +23,8 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 vim.opt.smarttab = true
 vim.opt.breakindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.wrap = true
 
 vim.opt.splitbelow = true
